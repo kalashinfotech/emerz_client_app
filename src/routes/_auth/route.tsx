803adc75435f212ebf7167c99e7171bc4685c345
Ankit <ajatkj.dev@gmail.com>
2026-01-31 09:19:09 +0530

    padding fix

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -8,7 +8,7 @@ function RouteComponent() {
   return (
     <>
       <div className="from-background via-background dark:to-primary-700/30 to-primary-100/30 flex min-h-screen w-full bg-radial-[at_50%_75%] to-90% md:h-screen">
-        <div className="flex h-full w-full items-center justify-center px-0 py-18 md:py-0">
+        <div className="flex h-full w-full items-center justify-center py-18 md:py-0">
           <Outlet />
         </div>
       </div>
